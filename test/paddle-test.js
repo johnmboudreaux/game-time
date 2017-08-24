100588c0e09e677b93b6c4bc9da9b9b7a9a8a065
johnmboudreaux <jhnbdrx@gmail.com>
2017-08-23 23:19:38 -0600

    removed some extra code

--- test/paddle-test.js
+++ test/paddle-test.js
@@ -1,34 +1,47 @@
 var {assert, expect, should} = require('chai');
 
 var Paddle = require('../lib/Paddle.js');
+var Ball = require('../lib/Ball.js');
 
 describe('paddle testing', function() {
   it('should be a function', () => {
-    var paddle = new Paddle();
+    // var paddle = new Paddle();
+
+    assert.isFunction(Paddle);
 
   })
 
-  it.skip('should have x coordinate', () => {
-    var paddle = new Paddle();
+  it('should have x coordinate', () => {
+    var paddle = new Paddle('x');
 
-
+    assert.equal(paddle.x, 'x');
   })
 
-  it.skip('should have y coordinate', () => {
-    var paddle = new Paddle();
+  it('should have y coordinate', () => {
+    var paddle = new Paddle('x', 'y');
 
+    assert.equal(paddle.y, 'y');
+    assert.isDefined(paddle.y);
   })
+
 
   it.skip('should have a width', () => {
     var paddle = new Paddle();
 
-    assert.equal(paddle.width, 40);
+
   })
 
   it.skip('should have a height', () => {
     var paddle = new Paddle();
 
-    assert.equal(paddle.height, 15)
+
+  })
+
+  it.skip('should collide with ball', () => {
+    var paddle = new Paddle();
+    var ball = new Ball();
+
+
   })
 
 })
